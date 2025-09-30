a79e0b2626d7b6725760d5a62512e4801e2895b8
uec-ascii <f2210542@gl.cc.uec.ac.jp>
2025-09-30 15:32:43 +0900

    不要なincludeを削除

--- src/plugin/contentverification/contentverification/content_verification_lib.h
+++ src/plugin/contentverification/contentverification/content_verification_lib.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <csmgrd/csmgrd_plugin.h>
 #include <cefore/cef_frame.h>
 
 int verify_content(unsigned char* msg, uint16_t msg_len);
