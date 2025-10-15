0f04374b6ee7c16aa0a416b8f6be91b3ddad00ab
uec-ascii <f2210542@gl.cc.uec.ac.jp>
2025-10-15 17:24:00 +0900

    Update cvfifo.c

--- src/csmgrd/plugin/lib/cvfifo/cvfifo.c
+++ src/csmgrd/plugin/lib/cvfifo/cvfifo.c
@@ -187,6 +187,17 @@ void
 insert (
 	CsmgrdT_Content_Entry* entry			/* content entry 							*/
 ) {
+    FILE *log_file = fopen("/tmp/content_verification.log", "a");
+    if (log_file == NULL) {
+        perror("ログファイルを開けませんでした");
+        return;
+    }
+    fprintf(log_file, "[Insert Entry] Name: ");
+    fwrite(entry->name, 1, entry->name_len, log_file);
+    fprintf(log_file, ":%u\nMsg: ", entry->chunk_num);
+    fwrite(entry->msg, 1, entry->msg_len, log_file);
+    fprintf(log_file, "\n");
+    fclose(log_file);
     if(verify_content(&content_map, entry->msg, entry->msg_len, entry->chunk_num) != 0){
         fprintf(stderr, "[FIFO LIB] content verification failed\n");
         return;
